2be7033ab8177984a60ef47ea249228799ab839b
ns <ns@betapay.online>
2023-02-01 17:50:46 +0300

    fix err

--- www/app.test/unitTest/test/domain/LocalTimeTest.php
+++ www/app.test/unitTest/test/domain/LocalTimeTest.php
@@ -37,11 +37,54 @@ class LocalTimeTest extends TestCase
         //Какую дату проверяем
         $targetTimestamp = 1678621920;
         $timeZonePeriod->setTargetDatetime((new DateTime())->setTimestamp($targetTimestamp));
-        $timeZonePeriod->setZoneEnd(new DateTime($data['zone_end']));
+        if($data['zone_end']) {
+            $timeZonePeriod->setZoneEnd(new DateTime($data['zone_end']));
+        }
         $timeZonePeriod->calcLocalDatetime();
 
         $expectedLocalTime = "2023-03-12 07:52:00";
         $expectedDst = true;
+        $localDatetime = $timeZonePeriod->getLocalDatetime();
+
+        $this->assertEquals($expectedLocalTime, $localDatetime->format("Y-m-d H:i:s"));
+        $this->assertEquals($expectedDst, $timeZonePeriod->getDst());
+
+    }
+
+    public function testCorrectLocalTimeWithoutEnd()
+    {
+        $data = [
+            "city_id" => "746bdf1d-d154-46cd-b104-9415fcc39e35",
+            "city_name" => "Midrand",
+            "zone_name" =>"Africa/Johannesburg",
+            "dst" => 0,
+            "gmt_offset" => "7200",
+            "zone_start" => "1995-10-16 04:00:00",
+            "zone_end" => null
+        ];
+
+
+        $city = new City( Uuid::fromString($data["city_id"]), $data['city_name']);
+        $gmtOffset = new GmtOffset($data['gmt_offset']);
+        $timeZonePeriod = new TimeZonePeriod(
+            $city,
+            $data['zone_name'],
+            new DateTime($data['zone_start']),
+            $gmtOffset,
+            $data['dst'],
+        );
+
+        //Какую дату проверяем
+        $targetTimestamp = 1678621920;
+        $timeZonePeriod->setTargetDatetime((new DateTime())->setTimestamp($targetTimestamp));
+        if($data['zone_end']) {
+            $timeZonePeriod->setZoneEnd(new DateTime($data['zone_end']));
+        }
+
+        $timeZonePeriod->calcLocalDatetime();
+
+        $expectedLocalTime = "2023-03-12 13:52:00";
+        $expectedDst = false;
         $localDatetime = $timeZonePeriod->getLocalDatetime();
 
         $this->assertEquals($expectedLocalTime, $localDatetime->format("Y-m-d H:i:s"));
@@ -75,7 +118,9 @@ class LocalTimeTest extends TestCase
         //Какую дату проверяем
         $targetTimestamp = 1678621920;
         $timeZonePeriod->setTargetDatetime((new DateTime())->setTimestamp($targetTimestamp));
-        $timeZonePeriod->setZoneEnd(new DateTime($data['zone_end']));
+        if($data['zone_end']) {
+            $timeZonePeriod->setZoneEnd(new DateTime($data['zone_end']));
+        }
         $timeZonePeriod->calcUtcDatetime();
 
         $expectedLocalTime = "2023-03-12 11:52:00";
